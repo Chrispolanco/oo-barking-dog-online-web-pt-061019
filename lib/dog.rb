e14d3930397ca69bb1e56528d61a49372ef0f65c
Christian Polanco <chrispolanco@sbcglobal.net>
2019-06-27 04:43:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-def bark
-  puts "Bark"
-end
+class Dog
+  def initialize 
+    bark
+  end
+
+  def bark
+    puts "Woof"
+  end
+
+  def name=(dog_name)
+    @no_name = dog_name
+  end
+
+  def name 
+    @no_name
+  end
+
+end 
